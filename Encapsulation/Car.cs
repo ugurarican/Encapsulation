8720a651ac42c11f30a5be88bdbd052a73002549
Ugur <ugur-arican@hotmail.com>
2025-01-11 22:25:16 +0300

    Add project files.

---
+++ Encapsulation/Car.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Car
+    {
+        //Fieldları tanımlıyoruz. Bizden sadece  kapı sayısını kapsüllememiz istendiği için onun erişim belirleyicisini private yapıyoruz.
+        public string brand;
+        public string model;
+        public string color;
+        private int _numberOfDoor;
+
+        //Property alanında sadece kapı sayısını yazıyoruz çünkü diğer değişkenlere dışarıdan erişime izin var.
+        public int NumberOfDoor
+        {
+            get { return _numberOfDoor; }
+
+
+            set
+            {
+                if (value == 2 || value == 4)
+                {
+                    _numberOfDoor = value;
+                }
+                else
+                {
+                    _numberOfDoor = -1;
+                }
+            }
+        }
+        //Parametreli Constructor ile marka, model, renk ve kapı sayısını girmeden girişe izin vermiyoruz.
+        public Car(string brand, string model, string color, int numberOfDoor)
+        {
+            this.brand = brand;
+            this.model = model;
+            this.color = color;
+            NumberOfDoor = numberOfDoor;
+        }
+
+        //Oluşturulan nesneleri gösteren metod.
+        public void DisplayCar()
+        {
+            Console.WriteLine($" Aracın Markası : {brand}" +
+                              $"\n Aracın Modeli : {model}" +
+                              $"\n Aracın Rengi: {color}" +
+                              $"\n Aracın Kapı Sayısı: {NumberOfDoor}" +
+                              $"\n                                   " +
+                              $"\n-----------------------------------" +
+                              $"\n                                   ");
+        }
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,16 @@
+﻿//Nesnemizi oluşturuyoruz.
+
+using Encapsulation;
+
+Car car1 = new Car("Merceses", "E-250", "Beyaz", 4);
+
+Car car2 = new Car("Honda", "S2000", "Mavi", 2);
+
+Car car3 = new Car("Mercedes", "E", "Beyaz", 6);
+
+//Oluşturduğumuz nesneleri görmek için metodumuzu çağırıyoruz.
+
+car1.DisplayCar();
+car2.DisplayCar();
+car3.DisplayCar();
+
